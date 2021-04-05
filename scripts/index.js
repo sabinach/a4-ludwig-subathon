6cacccfe3bce999f36a53ca5316c8eeaf5bad947
Sabina Chen <sabinach@mit.edu>
2021-04-05 18:06:07 -0400

    added zoomable highlights node

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 700 - margin.left - margin.right,
+    width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -58,15 +58,32 @@ function createViz(error, ...args) {
   console.log(desc_highlights)
   console.log(url_highlights)
 
+  const dummy_hours = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+  const dummy_values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+  const dummy_zip = dummy_hours.map((dummy_hour, index) => {
+    return {id: index, date: dummy_hour, value: dummy_values[index]}
+  });
+
+  console.log(dummy_zip);
+
+  // Define line
+  var highlightsLine = d3.line()
+    .defined(d => !isNaN(d.value))
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.value))
+
+
   /* --------------------------------------------- */
   // TIME LEFT
+
+  /* --- Simple Line DEFINITIONS --- */
 
   // reformat xy data (timeLeftJson)
   const timeStreamed_hours = Array.from(Array(timeLeftJson.timeLeft.length), (_,x) => x*0.5);
   const timeLeft_hours = timeLeftJson.timeLeft.map(d => parseTimeLeft(d));
   const timeLeftJson_zip = timeStreamed_hours.map((timeStreamed, index) => {
     return {timeStreamed:timeStreamed, timeLeft:timeLeft_hours[index] }
-  })
+  });
 
   // Define xy axes
   var xDomain = [0, d3.max(timeStreamed_hours)],
@@ -76,29 +93,11 @@ function createViz(error, ...args) {
   var xAxis = d3.axisBottom(xScale),
       yAxis = d3.axisLeft(yScale);
 
-  // Define brush
-  var brush = d3.brush().on("end", brushended),
-      idleTimeout,
-      idleDelay = 350;
-
   // Define line
-  var drawLine = d3.line()
+  var timeLeftLine = d3.line()
     .defined(d => !isNaN(d.timeLeft))
     .x(d => xScale(d.timeStreamed))
     .y(d => yScale(d.timeLeft))
-
-  // Add a clipPath: everything out of this area won't be drawn.
-  var clip = svg.append("defs").append("svg:clipPath")
-    .attr("id", "clip")
-    .append("svg:rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("x", 0)
-    .attr("y", 0);
-
-  // Create the line svg: where both the line and the brush take place
-  var svg_line = svg.append('g')
-    .attr("clip-path", "url(#clip)");
 
   // Add x-axis
   svg.append("g")
@@ -114,8 +113,7 @@ function createViz(error, ...args) {
   // Add x-axis label
   svg.append("text")             
     .attr("transform",
-          "translate(" + (width/2) + " ," + 
-                         (height + margin.top + 20) + ")")
+          "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
     .style("text-anchor", "middle")
     .text("# hours streamed");
 
@@ -128,19 +126,77 @@ function createViz(error, ...args) {
     .style("text-anchor", "middle")
     .text("# hours left");  
 
-  // Add brush
-  svg_line.append("g")
-    .attr("class", "brush")
-    .call(brush);
+  /* --- Brush + Line Clip DEFINITIONS --- */
 
-  // Add line
+  // Define brush
+  var brush = d3.brush().on("end", brushended),
+      idleTimeout,
+      idleDelay = 350;
+
+  // Add a clipPath: everything out of this area won't be drawn.
+  var clip = svg.append("defs").append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("x", 0)
+    .attr("y", 0);
+
+  // Create the line svg: where both the line and the brush take place
+  var svg_line = svg.append('g')
+    .attr("clip-path", "url(#clip)");
+
+  // Add line (timeLeft)
   svg_line.append("path")
     .datum(timeLeftJson_zip)
     .attr("class", "line")
     .attr("fill", "none")
     .attr("stroke", "steelblue")
     .attr("stroke-width", 1.5)
-    .attr("d", drawLine);
+    .attr("d", timeLeftLine);
+
+  // Add brush
+  svg_line.append("g")
+    .attr("class", "brush")
+    .call(brush);
+
+  // Add nodes (event highlights)
+  svg_line.selectAll("circle")
+    .data(dummy_zip)
+    .enter().append("circle")
+    .attr("cx", d => xScale(d.date))
+    .attr("cy", d =>  yScale(d.value))
+    .attr("r", (d, i) => 5)
+    .attr("id", d => d.id)
+    .style("fill", "#fcb0b5")
+    .on("mouseover", function(d) {
+
+      d3.select(this).transition().duration(200).style("fill", "#d30715");
+
+      svg_line.selectAll("#tooltip").data([d]).enter().append("text")
+        .attr("id", "tooltip")
+        .text((d, i) => d.value)
+        .attr("y", d => yScale(d.value)-12)
+        .attr("x", d => xScale(d.date))
+
+      svg_line.selectAll("#tooltip_path").data([d]).enter().append("line")
+        .attr("id", "tooltip_path")
+        .attr("class", "line")
+        .attr("d", highlightsLine)
+        .attr("x1", d => xScale(d.date))
+        .attr("x2", d => xScale(d.date))
+        .attr("y1", height)
+        .attr("y2", d => yScale(d.value))
+        .attr("stroke", "black")
+        .style("stroke-dasharray", ("3, 3"));
+    })
+    .on("mouseout", function (d) {
+      d3.select(this).transition().duration(500).style("fill", "#fcb0b5");
+      svg_line.selectAll("#tooltip").remove();
+      svg_line.selectAll("#tooltip_path").remove();
+    });
+
+  /* --- Brush + Line Clip FUNCTIONS --- */
 
   function brushended() {
     var brushBounds = d3.event.selection;
@@ -164,7 +220,10 @@ function createViz(error, ...args) {
     var t = svg.transition().duration(750);
     svg.select(".axis--x").transition(t).call(xAxis);
     svg.select(".axis--y").transition(t).call(yAxis);
-    svg_line.selectAll(".line").transition(t).attr("d", drawLine);
+    svg_line.selectAll(".line").transition(t).attr("d", timeLeftLine);
+    svg_line.selectAll("circle").transition(t)
+      .attr("cx", d => xScale(d.date))
+      .attr("cy", d => yScale(d.value));
   }
 
 };
